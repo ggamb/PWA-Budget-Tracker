409c3970a00b59062a7327afddc1947b2c15d8b4
ggamb <glenngambardella372@gmail.com>
2021-12-09 18:33:34 -0500

    added offline functionality

--- Assets/public/js/idb.js
+++ Assets/public/js/idb.js
@@ -3,6 +3,14 @@ let db;
 // establish a connection to IndexedDB database
 const request = indexedDB.open('budget_tracker', 1);
 
+// this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
+request.onupgradeneeded = function (event) {
+    // save a reference to the database 
+    const db = event.target.result;
+    // create an object store (table) called `new_pizza`, set it to have an auto incrementing primary key of sorts 
+    db.createObjectStore('new_transaction', { autoIncrement: true });
+};
+
 // upon a successful connection
 request.onsuccess = function (event) {
     // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
@@ -34,10 +42,13 @@ function saveRecord(record) {
 
 
 function uploadTransaction() {
+    //Open transaction on db
     const transaction = db.transaction(['new_transaction'], 'readwrite');
 
+    // access store object
     const transactionStore = transaction.objectStore('new_transaction');
 
+    //get all records from store and set to getAll variable
     const getAll = transactionStore.getAll();
 
     getAll.onsuccess = function () {
